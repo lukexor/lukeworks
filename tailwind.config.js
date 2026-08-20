1fbcdbe05489970fee5b9948f3a2adbd0e0aef99
Luke Petherbridge <lukexor@gmail.com>
2026-08-20 09:04:40 -0700

    chore: delete tailwind.config.js as no longer needed

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/* @type {import('tailwindcss').Config} */
-/* Tailwind 4.0 doesn't need a config but tailwindcss-intellisense still
- * requires it to work */
-module.exports = {
-  content: {
-    relative: true,
-    files: ["*.html", "./src/**/*.rs"],
-  },
-};
